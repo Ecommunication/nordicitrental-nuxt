75f0e818926c8993dbf23e6005ed44290b575963
Simon Duun <mail@simonduun.com>
2021-06-07 22:28:09 +0200

    Update nuxt.config.js
    Added noindex to general header meta tags

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,7 +7,8 @@ export default {
     },
     meta: [
       { charset: "utf-8" },
-      { name: "viewport", content: "width=device-width, initial-scale=1" }
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      { hid: 'robots', name: 'robots', content: 'noindex, nofollow' }
     ],
     link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }]
   },
