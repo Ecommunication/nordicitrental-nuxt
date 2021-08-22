ed02f142b7cda2d11707f65a3feaec5eacfd1e1a
Simon Duun <simon@eindom.dk>
2021-08-22 21:49:57 +0200

    remove noidnex

--- nuxt.config.js
+++ nuxt.config.js
@@ -26,8 +26,7 @@ export default {
     },
     meta: [
       { charset: "utf-8" },
-      { name: "viewport", content: "width=device-width, initial-scale=1" },
-      { hid: "robots", name: "robots", content: "noindex, nofollow" }
+      { name: "viewport", content: "width=device-width, initial-scale=1" }
     ],
     link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }]
   },
