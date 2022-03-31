e8d616a0c27c1a3183afade487feec1847766372
Mikkel Madsen <mm@apptitude.dk>
2022-03-31 09:00:50 +0200

    added new styles for currency & language as requested by Michael.

--- components/Header/Cta.vue
+++ components/Header/Cta.vue
@@ -1,44 +1,83 @@
 <i18n>
 {
   "en": {
-    "language":"Choose language: ",
-    "currency":"Choose currency: "
+    "language":"Choose language",
+    "currency":"Choose currency"
   },
   "da": {
-    "language":"Vælg sprog: ",
-    "currency":"Vælg valuta: "
+    "language":"Vælg sprog",
+    "currency":"Vælg valuta"
   }
 }
 </i18n>
 
 <template>
-  <div class="cta-header mb-2 flex w-full justify-between">
-    <div class="flex flex-col space-x-6 md:flex-row">
-      <p class="text-sm font-medium text-mainBlue">
-        {{ $t('language') }}
-        <nuxt-link
-          class="font-bold"
-          v-for="locale in availableLocales"
-          :key="locale.code"
-          :to="switchLocalePath(locale.code)"
-          >{{ locale.name }}</nuxt-link
-        >
-      </p>
-      <button @click="currencyClick" class="text-sm font-bold text-mainBlue">
-        <span class="font-medium">{{ $t('currency') }}</span
-        ><span>{{ this.currency }}</span>
-      </button>
-    </div>
+  <div class="cta-header mb-2 flex w-full space-x-8">
     <span class="cta my-auto">
       <i class="fas fa-phone mr-2"></i>
       {{ cta }}
     </span>
+    <div class="relative flex flex-col space-x-6 md:flex-row">
+      <div
+        class="group my-auto flex cursor-pointer space-x-2 text-mainBlue"
+        @click="langClick"
+      >
+        <i class="fas fa-globe-europe fa-lg text-center"></i>
+        <i
+          :class="`fas fa-chevron-down transform transition-transform duration-300 ${
+            langOpen && 'rotate-180'
+          } group-hover:text-opacity-50`"
+        ></i>
+        <div
+          v-if="langOpen"
+          class="absolute top-8 z-20 rounded-sm bg-white p-2 shadow-md"
+        >
+          <div class="flex flex-col">
+            <nuxt-link
+              class="text-base font-medium"
+              v-for="locale in availableLocales"
+              :key="locale.code"
+              :to="switchLocalePath(locale.code)"
+              >{{ locale.name }}</nuxt-link
+            >
+          </div>
+        </div>
+      </div>
+      <div
+        class="group my-auto flex cursor-pointer space-x-2 text-mainBlue"
+        @click="currencyClick"
+      >
+        <div
+          class="flex h-8 w-8 justify-center rounded-full border-2 border-mainBlue"
+        >
+          <i class="fas fa-euro-sign fa-sm my-auto mr-1"></i>
+        </div>
+        <i
+          :class="`fas fa-chevron-down my-auto transform transition-transform duration-300 ${
+            currencyOpen && 'rotate-180'
+          } group-hover:text-opacity-50`"
+        ></i>
+        <div
+          v-if="currencyOpen"
+          class="absolute top-8 z-20 bg-white p-2 shadow-md"
+        >
+          <div class="flex flex-col">
+            <button class="text-base font-medium" @click="changeCurrency">
+              {{ this.currency }}
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 <script>
 import { mapState, mapActions } from 'vuex';
 export default {
   props: ['cta'],
+  data() {
+    return { langOpen: false, currencyOpen: false };
+  },
   computed: {
     ...mapState(['currency']),
     availableLocales() {
@@ -48,7 +87,11 @@ export default {
   methods: {
     ...mapActions(['changeCurrency']),
     currencyClick() {
-      this.changeCurrency();
+      // this.changeCurrency();
+      this.currencyOpen = !this.currencyOpen;
+    },
+    langClick() {
+      this.langOpen = !this.langOpen;
     },
   },
 };
