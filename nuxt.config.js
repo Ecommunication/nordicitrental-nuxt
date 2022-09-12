1e4a75dfb63c381509675af6f970ad9450d8fe94
Simon Duun <sda@iMac.NordicNet>
2022-09-12 10:48:57 +0200

    upload junk

--- nuxt.config.js
+++ nuxt.config.js
@@ -58,6 +58,7 @@ export default {
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
+    '~/plugins/dom.js',
     '~/plugins/globals.js',
     '~/plugins/filters/prices.js',
     '~/plugins/filters/date.js',
